15bf82667674cf66aa1aa57bdd6bd59623e92b5c
fatimaatk <aitkhelifa.fatima59@gmail.com>
2021-10-28 16:10:38 +0200

    mise en place des boutons de recherche sans lien api home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Search from './components/Search/Search';
 import './app.css';
 import ProductList from './components/ProductList/ProductList';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <main className="main">
+        <Search />
         <ProductList />
       </main>
       <Footer />

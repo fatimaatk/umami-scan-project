8e4be6b1af13a82abc6121f5395c741f5dc57df9
Julien Roth <julien@infraroth.com>
2021-10-28 16:10:26 +0200

    Added routes and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './app.css';
 import ProductList from './components/ProductList/ProductList';
+import MyUmami from './components/MyUmami/MyUmami';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main className="main">
-        <ProductList />
-      </main>
+      <Router>
+        <Header />
+        <main className="main">
+          <Switch>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <Route path="/my-umami">
+              <MyUmami />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
       <Footer />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,53 @@
-import Menu from './Menu/Menu';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo_umami.svg';
 import './header.css';
 
 const Header = () => {
+  const [hamburgerOpened, setHamburgerOpened] = useState(false);
+  const HandleHamburger = (e) => {
+    e.preventDefault();
+    setHamburgerOpened(!hamburgerOpened);
+  };
   return (
-    <header className="header">
-      <div>
-        <img className="logo" src={Logo} alt="logo Umami" />
+    <header className={`header ${hamburgerOpened ? 'hamburger-opened' : ''}`}>
+      <div className="header-container">
+        <Link to="/">
+          <img
+            className="logo"
+            src={Logo}
+            alt="logo Umami"
+            width="150"
+            height="41"
+          />
+        </Link>
+        <div
+          onClick={HandleHamburger}
+          onKeyPress={HandleHamburger}
+          className="hamburger"
+          role="button"
+          tabIndex={0}
+        >
+          <span className="hamburger-span">&nbsp;</span>
+          <span className="hamburger-span">&nbsp;</span>
+          <span className="hamburger-span">&nbsp;</span>
+          <span className="hamburger-span">&nbsp;</span>
+        </div>
+
+        <nav>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/my-umami">My Umami</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div className="hamburger">
-        <span className="hamburger-span">&nbsp;</span>
-        <span className="hamburger-span">&nbsp;</span>
-        <span className="hamburger-span">&nbsp;</span>
-        <span className="hamburger-span">&nbsp;</span>
-      </div>
-      <>
-        <Menu />
-      </>
     </header>
   );
 };

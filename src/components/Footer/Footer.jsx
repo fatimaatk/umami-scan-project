b33e66d518f3b0d864e701af66d4cb1a120aa7f4
fatimaatk <aitkhelifa.fatima59@gmail.com>
2021-11-05 12:29:29 +0100

    maj lien linkedin fatima et julien

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,22 +3,51 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-
       <ul className="social_list">
         <li className="contactTeam">
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+          >
             Aurelien Ferrand
+          </a>
         </li>
         <li className="contactTeam">
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+          >
             Fatima Ait Khelifa
+          </a>
         </li>
         <li className="contactTeam">
+          {' '}
+          <a
+            href="https://www.linkedin.com/in/julienroth/?originalSubdomain=fr"
+            target="_blank"
+            rel="noreferrer"
+          >
             Julien Roth
+          </a>
         </li>
         <li className="contactTeam">
+          {' '}
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+          >
             Nicolas Bas
+          </a>
         </li>
       </ul>
-      <p><br/>Copyright @2021 | Designed by Umami Fr Remote <br/> <br/>Project designed for Web Developper training at the WildCodeSchool</p>
+      <p>
+        <br />
+        Copyright @2021 | Designed by Umami Fr Remote <br /> <br />
+        Project designed for Web Developper training at the WildCodeSchool
+      </p>
     </footer>
   );
 };
